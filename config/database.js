088f72b31b00f6b904954132be490871732a7858
박종혁 <Jonghyeok@Jonghyeok-MacBook-Pro.local>
2015-12-19 16:48:50 +0900

    jonghyeok

---
+++ config/database.js
@@ -0,0 +1,11 @@
+// config/database.js
+module.exports = {
+    'connection': {
+        'host': 'www.pongpongapp.com',
+        'port': '3306',
+        'user': 'akindo19',
+        'password': 'khjpjh0514'
+    },
+	'database': 'akindo19',
+	'users_table': 'msv_users'
+};
